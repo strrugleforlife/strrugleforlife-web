124244313c9144d79f85cd48bafce92b66a9fdbe
strrugleforlife <kuro.y6222@gmail.com>
2020-03-13 22:19:06 +0900

    test

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/sprin-boot-sample-1.0.jar
--- src/main/java/jp/co/strrugleforlife/StrrugleforlifeWebApplication.java
+++ src/main/java/jp/co/strrugleforlife/StrrugleforlifeWebApplication.java
@@ -1,13 +1,25 @@
 package jp.co.strrugleforlife;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
+@EnableAutoConfiguration
 public class StrrugleforlifeWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StrrugleforlifeWebApplication.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello, World!";
+    }
+
+    public static void main(String[] arguments) {
+        SpringApplication.run(StrrugleforlifeWebApplication.class, arguments);
+    }
 
 }
