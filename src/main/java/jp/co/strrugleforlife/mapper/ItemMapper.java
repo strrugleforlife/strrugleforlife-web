7959ba6f4a71d636cc2f54c250d078b1b16d78e7
strrugleforlife <kuro.y6222@gmail.com>
2020-03-14 00:10:27 +0900

    test

---
+++ src/main/java/jp/co/strrugleforlife/mapper/ItemMapper.java
@@ -0,0 +1,21 @@
+package jp.co.strrugleforlife.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import jp.co.strrugleforlife.domain.Item;
+
+
+@Mapper
+public interface ItemMapper {
+  List<Item> findAll();
+
+  Item findOne(Long id);
+
+  void save(Item item);
+
+  void update(Item item);
+
+  void delete(Long id);
+}
